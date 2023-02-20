38212a79b23703349b894ec03248281374a12698
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-20 21:56:09 +0200

    Update TodoForm.js

--- app/src/components/TodoForm.js
+++ app/src/components/TodoForm.js
@@ -4,15 +4,26 @@ import { useState } from "react";
 function TodoForm() {
   const [input, setInput] = useState("");
 
+  const handleChange = (ev) => {
+    setInput(ev.target.value);
+  };
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+
+    setInput("");
+  };
+
   return (
     <div>
-      <form className="todo-form">
+      <form className="todo-form" onSubmit={onSubmit}>
         <input
           type="text"
           placeholder="Add to do..."
           value={input}
           name="text"
           className="todo-input"
+          onChange={handleChange}
         />
         <button className="todo-button">Add</button>
       </form>
