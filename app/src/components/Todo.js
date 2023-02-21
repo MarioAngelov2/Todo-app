8a41596462f05593796b82a368efc4e86d7c6a30
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-21 21:08:43 +0200

    Update Todo.js

--- app/src/components/Todo.js
+++ app/src/components/Todo.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { useState } from "react";
 import TodoForm from "./TodoForm";
-import {AiOutlineCloseCircle} from 'react-icons/ai'
-import {TiEdit} from 'react-icons/ti'
+import { AiOutlineCloseCircle } from "react-icons/ai";
+import { TiEdit } from "react-icons/ti";
 
-
-function Todo({todos, completeTodo}) {
+function Todo({ todos, completeTodo, removeTodo }) {
   const [edit, setEdit] = useState({
     id: null,
     value: "",
@@ -16,15 +15,23 @@ function Todo({todos, completeTodo}) {
       className={todo.isComplete ? "todo-row complete" : "todo-row"}
       key={index}
     >
-
-    <div key={todo.id} onClick={() => completeTodo(todo.id)}>
-      {todo.text}
-    </div>
-    <div className="icons">
-
-    </div>
-      <AiOutlineCloseCircle />
-      <TiEdit />
+      <div key={todo.id} onClick={() => completeTodo(todo.id)}>
+        {todo.text}
+      </div>
+      <div className="icons"></div>
+      <AiOutlineCloseCircle
+        onClick={() => {
+          removeTodo(todo.id);
+        }}
+        className="delete-icon"
+      />
+      <TiEdit
+        onClick={() => {
+          setEdit(() => setEdit({id: todo.id, value: todo.text}))
+        }
+      }
+        className="edit-icon"
+      />
     </div>
   ));
 }
