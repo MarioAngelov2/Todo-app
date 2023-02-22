cf6ddfe78f167cb1a77590ee0a2040fe2b4650a4
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-22 21:56:00 +0200

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-import CreateTodo from './components/CreateTodo';
-
+import "./App.css";
+import CreateTodo from "./components/CreateTodo";
+import TodoItem from "./components/TodoItem";
 
 function App() {
   return (
     <div className="App">
-      <CreateTodo />
+      <header>
+        <h1>TODO App</h1>
+      </header>
+      <main>
+        <CreateTodo />
+      </main>
     </div>
   );
 }
