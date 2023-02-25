d1be47ece648e414780128f28803fc2f88a09664
Mario Angelov <107209439+MarioAngelov2@users.noreply.github.com>
2023-02-25 11:41:06 +0200

    Update TodoItem.js

--- app/src/components/TodoItem.js
+++ app/src/components/TodoItem.js
@@ -2,13 +2,13 @@ import React from "react";
 import { useContext } from "react";
 import { TodoContext } from "../context/TodoContext";
 
-function TodoItem({ title, todoId }) {
+function TodoItem({ todo }) {
   const { taskDeleteHandler } = useContext(TodoContext);
 
   return (
     <li>
-      {title}
-      <button onClick={() => taskDeleteHandler(todoId)}>x</button>
+      {todo.title}
+      <button onClick={() => taskDeleteHandler(todo._id)}>x</button>
     </li>
   );
 }
